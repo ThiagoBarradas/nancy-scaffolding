1aab35064ede378a9665d02c7a786b034714436a
Thiago Barradas <thiagobrowskt@gmail.com>
2019-01-31 17:50:54 -0200

    :art: add describe pdf resource

---
+++ Nancy.Scaffolding/Docs/Attributes/DescribePdfResourceAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Nancy.Scaffolding.Docs.Attributes
+{
+    public class DescribePdfResourceAttribute : DescribeResourceBase
+    {
+        public DescribePdfResourceAttribute(Type controllerType, string methodName)
+            : base(controllerType, methodName, "application/pdf", "text/plain")
+        { }
+    }
+}
