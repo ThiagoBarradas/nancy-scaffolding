5e5e66898b48b249bc03aac2729b7e82907070be
Thiago Barradas <thiagobrowskt@gmail.com>
2019-03-12 19:24:10 -0300

    add describe resource for xml

---
+++ Nancy.Scaffolding/Docs/Attributes/DescribeXmlResourceAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Nancy.Scaffolding.Docs.Attributes
+{
+    public class DescribeXmlResourceAttribute : DescribeResourceBase
+    {
+        public DescribeXmlResourceAttribute(Type controllerType, string methodName)
+            : base(controllerType, methodName, "application/xml", "application/xml")
+        { }
+    }
+}
